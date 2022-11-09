bff85b2e85f1ce50262b8cce14cfea38f33491d7
jadson-medeiros <developer.medeiros@gmail.com>
2022-11-08 22:09:57 -0300

    added producer

---
+++ cmd/producer/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"math/rand"
+
+	"github.com/google/uuid"
+	amqp "github.com/rabbitmq/amqp091-go"
+
+	"github.com/jadson-medeiros/go-intensive/internal/order/entity"
+)
+
+func Publish(ch *amqp.Channel, order entity.Order) error {
+	body, err := json.Marshal(order)
+	if err != nil {
+		return err
+	}
+	err = ch.Publish(
+		"amq.direct",
+		"",
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "application/json",
+			Body:        body,
+		},
+	)
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func GenerateOrders() entity.Order {
+	return entity.Order{
+		ID:    uuid.New().String(),
+		Price: rand.Float64() * 100,
+		Tax:   rand.Float64() * 10,
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+	ch, err := conn.Channel()
+
+	if err != nil {
+		panic(err)
+	}
+	defer ch.Close()
+
+	for i := 0; i < 100; i++ {
+		Publish(ch, GenerateOrders())
+	}
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/jadson-medeiros/go-intensive
 go 1.18
 
 require (
+	github.com/google/uuid v1.3.0
 	github.com/mattn/go-sqlite3 v1.14.16
 	github.com/rabbitmq/amqp091-go v1.5.0
 	github.com/stretchr/testify v1.8.1
