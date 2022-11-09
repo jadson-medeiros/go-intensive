c445c4d357c8f197ac9bdbdb9fb2f53087c4d944
jadson-medeiros <developer.medeiros@gmail.com>
2022-11-08 21:42:59 -0300

    update gitignore

--- internal/order/infra/database/order_repository_test.go
+++ internal/order/infra/database/order_repository_test.go
@@ -19,7 +19,7 @@ type OrderRepositoryTestSuite struct {
 func (suite *OrderRepositoryTestSuite) SetupSuite() {
 	db, err := sql.Open("sqlite3", ":memory:")
 	suite.NoError(err)
-	db.Exec("CREATE TABLE orders (id varchar(255) NOT NULL, price float NOT NULL, tax float NOT NULL, final_price float NOT NULL);")
+	db.Exec("CREATE TABLE orders (id varchar(255) NOT NULL, price float NOT NULL, tax float NOT NULL, final_price float NOT NULL, PRIMARY KEY (id));")
 	suite.Db = db
 }
 
